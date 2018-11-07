60434d3946470116ff089e5a885db5e3b87250d6
dashanutikova <dashanutikova@gmail.com>
2018-11-07 12:56:25 +0700

    wednesday

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, escape, session
+from flask import Flask, render_template, request, escape, session, copy_current_request_context
 from vsearch import search4letters
 from DBcm import UseDatabase, ConnectorError, CredentialError, SQLError
 from checker import check_logged_in
+from threading import Thread
+from time import sleep
 
 app = Flask(__name__)
 
@@ -11,32 +13,34 @@ app.config['dbconfig'] = {'host': '127.0.0.1',
 			'user':'loh',
 			'password':'loh',
 			'database':'vsearchlogDB',	}
-
-def log_request(req:'flask request', res:str) -> None:
-
-	with UseDatabase(app.config['dbconfig']) as cursor:
-		_SQL = """insert into log
-		(phrase, letters, ip, browser_string,results)
-		values
-		(%s, %s, %s, %s, %s)"""
-		cursor.execute(_SQL, (req.form['phrase'],
-							req.form['letters'],
-							req.remote_addr,
-							req.user_agent.browser,
-							res, ))
 		
 @app.route('/search4',methods = ['POST'])
 def do_search() ->'html':
+
+	@copy_current_request_context
+	def log_request(req:'flask request', res:str) -> None:
+		sleep(15)
+		with UseDatabase(app.config['dbconfig']) as cursor:
+			_SQL = """insert into log
+			(phrase, letters, ip, browser_string,results)
+			values
+			(%s, %s, %s, %s, %s)"""
+			cursor.execute(_SQL, (req.form['phrase'],
+								req.form['letters'],
+								req.remote_addr,
+								req.user_agent.browser,
+								res, ))
 	phrase = request.form['phrase']
 	letters = request.form['letters']
 	title = 'Here are your results'
 	results = str(search4letters(phrase, letters))
 	try:
-		log_request(request,results)
+		t = Thread(target = log_request, args = (request,results))
+		t.start()
 	except Exception as err:
 		print('*****Login failed with this error', str(err))
 	return render_template('results.html', the_title=title, the_phrase = phrase, the_letters = letters, the_results = results)
-
+	
 @app.route('/viewlog')
 @check_logged_in
 def view_the_log() -> 'html':
